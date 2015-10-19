d2b345fabd20136438c27ab1a8a8da21a97c6fde
Joseph McCausland <jmccausland@esri.com>
2015-10-19 10:05:02 -0400

    Hook up map tool button with input box

--- source/CoordinateTool/ArcMapAddinCoordinateTool/CoordinateToolButton.cs
+++ source/CoordinateTool/ArcMapAddinCoordinateTool/CoordinateToolButton.cs
@@ -45,99 +45,33 @@ namespace ArcMapAddinCoordinateTool
 
         }
 
-
-        #region"MapControl Left and Right Mouse Clicks"
-        // ArcGIS Snippet Title:
-        // MapControl Left and Right Mouse Clicks
-        // 
-        // Long Description:
-        // Stub code for using left and right mouse clicks for ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseDownEvent.
-        // 
-        // Add the following references to the project:
-        // ESRI.ArcGIS.AxControls
-        // ESRI.ArcGIS.Controls
-        // 
-        // Intended ArcGIS Products for this snippet:
-        // ArcGIS Engine
-        // 
-        // Applicable ArcGIS Product Versions:
-        // 9.2
-        // 9.3
-        // 9.3.1
-        // 10.0
-        // 
-        // Required ArcGIS Extensions:
-        // (NONE)
-        // 
-        // Notes:
-        // This snippet is intended to be inserted at the base level of a Class.
-        // It is not intended to be nested within an existing Method.
-        // 
-
-        ///<summary>Stub code for using left and right mouse clicks for ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseDownEvent.</summary>
-        /// 
-        ///<param name="e">An ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseDownEvent.</param>
-        /// 
-        ///<remarks>e is obtained as the result of the AxMapControl.OnMouseDown event.</remarks>
-        //public void MapControlLeftAndRightMouseClicks(ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseDownEvent e)
-        //{
-
-        //    if (e.button == 1) // Left mouse click
-        //    {
-
-        //        // TODO: Your implementation code.....
-
-        //    }
-
-        //    if (e.button == 2) // Right mouse click
-        //    {
-
-        //        // TODO: Your implementation code.....
-
-        //    }
-
-        //}
-        #endregion
-
         protected override void OnMouseDown(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
         {
-            var doc = AddIn.FromID<ArcMapAddinCoordinateTool.DockableWindowCoordinateTool.AddinImpl>(ThisAddIn.IDs.DockableWindowCoordinateTool);
-
-            if(doc != null)
+            if (arg.Button != System.Windows.Forms.MouseButtons.Left)
+                return;
+            try
             {
-                doc.TestMe();
+                //Get the active view from the ArcMap static class.
+                IActiveView activeView = ArcMap.Document.ActiveView;
+
+                var point = activeView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y) as IPoint;
+
+                // always use WGS84
+                var sr = GetSR();
+
+                if (sr != null)
+                {
+                    point.Project(sr);
+                }
+
+                var doc = AddIn.FromID<ArcMapAddinCoordinateTool.DockableWindowCoordinateTool.AddinImpl>(ThisAddIn.IDs.DockableWindowCoordinateTool);
+
+                if (doc != null)
+                {
+                    doc.SetInput(point.X, point.Y);
+                }
             }
-            //Get the active view from the ArcMap static class.
-            IActiveView activeView = ArcMap.Document.ActiveView;
-            var mc2 = activeView.FocusMap as IMapControl2;
-            if (mc2 != null)
-            {
-                var point2 = mc2.ToMapPoint(arg.X, arg.Y);
-            }
-
-            var point = activeView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y) as IPoint;
-
-            var sr = GetSR();
-
-            point.Project(sr);
-
-            Console.WriteLine(string.Format("{0} {1}", point.X, point.Y));
-
-            double x = point.X;
-            double y = point.Y;
-
-            var temp = activeView.FocusMap.MapUnits;
-
-            //If it's a polyline object, get from the user's mouse clicks.
-            //IPolyline polyline = GetPolylineFromMouseClicks(activeView);
-            ////Make a color to draw the polyline. 
-
-            //IRgbColor rgbColor = new RgbColorClass();
-            //rgbColor.Red = 255;
-            ////Add the user's drawn graphics as persistent on the map.
-            //AddGraphicToMap(activeView.FocusMap, polyline, rgbColor, rgbColor);
-            ////Best practice: Redraw only the portion of the active view that contains graphics. 
-            //activeView.PartialRefresh(esriViewDrawPhase.esriViewGraphics, null, null);
+            catch { }
         }
 
         private ISpatialReference GetSR()
