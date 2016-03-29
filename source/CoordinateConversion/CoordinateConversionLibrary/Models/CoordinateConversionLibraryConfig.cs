65283d8116f7f3e618002ec10a888b145d4dfa5c
Joseph McCausland <jmccausland@esri.com>
2016-03-29 10:32:23 -0400

    Add config class

---
+++ source/CoordinateConversion/CoordinateConversionLibrary/Models/CoordinateConversionLibraryConfig.cs
@@ -0,0 +1,29 @@
+﻿using CoordinateConversionLibrary.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoordinateConversionLibrary.Models
+{
+    public class CoordinateConversionLibraryConfig : NotificationObject
+    {
+        public CoordinateConversionLibraryConfig()
+        {
+
+        }
+
+        private CoordinateTypes displayCoordinateType = CoordinateTypes.None;
+        public CoordinateTypes DisplayCoordinateType
+        {
+            get { return displayCoordinateType; }
+            set
+            {
+                displayCoordinateType = value;
+                RaisePropertyChanged(() => DisplayCoordinateType);
+                Mediator.NotifyColleagues(Constants.DISPLAY_COORDINATE_TYPE_CHANGED, null);
+            }
+        }
+    }
+}
