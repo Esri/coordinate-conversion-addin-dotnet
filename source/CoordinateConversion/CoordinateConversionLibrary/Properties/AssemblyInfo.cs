962ac098653c70919945910239beab508739387e
Chris Moore <csmooreacm@acm.org>
2017-05-24 17:38:54 -0400

    Update version to 2.1

--- source/CoordinateConversion/CoordinateConversionLibrary/Properties/AssemblyInfo.cs
+++ source/CoordinateConversion/CoordinateConversionLibrary/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CoordinateConversionLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("CoordinateConversionLibrary")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Esri")]
 [assembly: AssemblyProduct("CoordinateConversionLibrary")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © Esri 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
