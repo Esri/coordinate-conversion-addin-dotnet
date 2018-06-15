4b874548bcf7a6c2f571e251d1acc4ca66d89eff
SaiP <Sai.Phaneendra@cybertech.com>
2018-06-15 18:07:18 +0530

    This commit addresses the UAT feedback provided in pull request #497

--- source/CoordinateConversion/CoordinateConversionLibrary/Views/EditOutputCoordinateView.xaml.cs
+++ source/CoordinateConversion/CoordinateConversionLibrary/Views/EditOutputCoordinateView.xaml.cs
@@ -19,6 +19,7 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using CoordinateConversionLibrary.Models;
 using CoordinateConversionLibrary.ViewModels;
+using System.Text.RegularExpressions;
 
 namespace CoordinateConversionLibrary.Views
 {
@@ -59,21 +60,42 @@ namespace CoordinateConversionLibrary.Views
         {
             var vm = this.DataContext as EditOutputCoordinateViewModel;
 
+            Regex alphanumericRegex = new Regex("^[a-zA-Z0-9]*$");
+            Regex nonNumericStartRegex = new Regex("^(?![0-9])");
+            Regex characterLimitRegex = new Regex("^[a-zA-Z0-9]{0,10}?$");
+
             if (vm == null)
                 return;
 
-            if(vm.Names.Contains(vm.OutputCoordItem.Name))
+            if (vm.Names.Contains(vm.OutputCoordItem.Name))
             {
                 // no duplicates please
                 e.Handled = false;
                 MessageBox.Show(string.Format("The name '{0}' is already used.", vm.OutputCoordItem.Name));
                 return;
             }
-
-            if(string.IsNullOrWhiteSpace(vm.OutputCoordItem.Name))
+            else if (string.IsNullOrWhiteSpace(vm.OutputCoordItem.Name))
             {
                 e.Handled = false;
                 MessageBox.Show("Name is required.");
+                return;
+            }
+            else if (!alphanumericRegex.IsMatch(vm.OutputCoordItem.Name))
+            {
+                e.Handled = false;
+                MessageBox.Show("The name should only contain alphabet and numbers.");
+                return;
+            }
+            else if (!nonNumericStartRegex.IsMatch(vm.OutputCoordItem.Name))
+            {
+                e.Handled = false;
+                MessageBox.Show("The name should not start with a number.");
+                return;
+            }
+            else if (!characterLimitRegex.IsMatch(vm.OutputCoordItem.Name))
+            {
+                e.Handled = false;
+                MessageBox.Show("The name must be 10 characters or less.");
                 return;
             }
 
